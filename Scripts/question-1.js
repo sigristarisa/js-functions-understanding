e9db3b32648ba9de98ff7ec25d97381bd7047d64
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-16 15:58:33 +0100

    has the answer for question-2.js

--- Scripts/question-1.js
+++ Scripts/question-1.js
@@ -5,12 +5,13 @@ function myFunction(num1, num2) {
 }
 
 const result = myFunction(5, 5);
-console.log(result);
 
 /*** ANSWER *** 
-The result will be 10.
+ The result will be 10.
+ 
+ Why: the function "myFunction(num1, num2)" is initialized as an variable called "result". 
+ The function "myFunction(num1, num2)" returns adding num1 and num2.
+ As the "myFunction(num1, num2)" has two arguments (5, 5), it returns 5 + 5 when the result is called
+ */
 
-Why: the function "myFunction(num1, num2)" is initialized as an variable called "result". 
-The function "myFunction(num1, num2)" returns adding num1 and num2.
-As the "myFunction(num1, num2)" has two arguments (5, 5), it returns 5 + 5 when the result is called
-*/
+console.log(result);
---
+++ Scripts/question-2.js
@@ -0,0 +1,17 @@
+// Q2. What is the value of result after calling this function? Why?
+
+function myFunction(num1, num2) {
+  num1 + num2;
+}
+
+const result = myFunction(5, 5);
+
+/*** ANSWER *** 
+ The result will be undefined.
+ 
+ Why: the function "myFunction(num1, num2)" is initialized as an variable called "result". 
+ The function "myFunction(num1, num2)" adds num1 and num2, but will not return it.
+ As the "myFunction(num1, num2)" has two arguments (5, 5) but will not return anything, it returns undefined when the result is called
+ */
+
+console.log(result);
