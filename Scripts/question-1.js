4788583cf441b30cccefb45ce47ea0ecc777dc28
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-16 15:31:56 +0100

    initial commit

---
+++ Scripts/question-1.js
@@ -0,0 +1,14 @@
+// Q1. What is the value of result after calling this function? Why?
+
+function myFunction(num1, num2) {
+  return num1 + num2;
+}
+
+const result = myFunction(5, 5);
+
+/*  ANSWER 
+The result will be 10.
+Why: 
+
+
+*/
