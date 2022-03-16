1f8a2216668802e060ff3d6ba1498a247a77bbfb
sigristarisa <arisasigrist.ch@gmail.com>
2022-03-16 17:46:43 +0100

    has the answer of question-22.js

--- Scripts/question-22.js
+++ Scripts/question-22.js
@@ -1,21 +1,21 @@
-// Q21. What will be the value of result when this code runs? Why?
+// Q22. What will be the value of result when this code runs? Why?
 
 function myFunction(num1, num2) {
-  const result = num1 + num2;
+  let result = num1 + num2;
   return result;
 }
 
-const result = myFunction(10, 20);
+let result = 0;
 myFunction(100, 2);
 
 /*** ANSWER *** 
-The value of result will be 30.
+The value of result will be 0.
 
  Why: The function "myFunction(num1, num2)" has two parameter.
  Within the function, a variable "result" is declared and initialized the value of the sum of num1 and num2.
- Out of the function, a variables "result" is declared and initialized with a value of myFunction(10, 20).
+ Out of the function, a variables "result" is declared and initialized with a value of 0.
  Afterwards, "myFunction()" is declared with two arguments 100, 2.
-The variable "result" will have 30 as a valuable, as it returns the sum of its arguments (10 + 20).
+The variable "result" will have 0 as a valuable, as its value has been reassigned to 0.
  
 Result: as expected.
  */
